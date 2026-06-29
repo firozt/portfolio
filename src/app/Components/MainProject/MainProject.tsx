0e03d871983866d41530d4442d67038b572f8084
firozt <firozt03@gmail.com>
2026-06-29 18:41:16 +0100

    build err

--- src/app/Components/MainProject/MainProject.tsx
+++ src/app/Components/MainProject/MainProject.tsx
@@ -1,28 +1,13 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./index.css";
 
 export const MainProject = () => {
-  const ref = useRef(null);
   const [expanded, setExpanded] = useState(false);
-
-  const handleClick = () => {
-    setExpanded((prev) => !prev);
-
-    // wait for layout update
-    requestAnimationFrame(() => {
-      ref.current?.scrollIntoView({
-        behavior: "smooth",
-        inline: "center",
-        block: "nearest",
-      });
-    });
-  };
 
   return (
     <div
-      ref={ref}
       className={`main-proj ${expanded ? "expanded" : ""}`}
-      onClick={handleClick}
+      onClick={() => setExpanded(!expanded)}
     >
       <h1>Hello</h1>
     </div>
